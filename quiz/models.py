acc3c9690842086291b97a32788e05ee227853ce
Abhishek Banerjee <abhishek.banerjee18@outlook.com>
2020-11-24 20:15:16 +0530

    Test for git prune..ignore!

--- quiz/models.py
+++ quiz/models.py
@@ -67,3 +67,4 @@ class Answer(Updated):
     question = models.ForeignKey(Question, related_name='answer', on_delete=models.DO_NOTHING)
     answer_text = models.CharField(max_length=255, verbose_name=_("Answer Text"))
     is_right = models.BooleanField(default=False)
+    #test
